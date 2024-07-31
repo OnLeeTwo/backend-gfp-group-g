837979916c23fee71559e4ef9c9d2309055123d0
Owent Ovandy <owent.ovandy7777@gmail.com>
2024-07-31 18:12:19 +0700

    added login validation and modified user validation

--- validations/user_register.py
+++ validations/user_register.py
@@ -5,4 +5,5 @@ user_register_schema = {
         "regex": "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
     },
     "password": {"type": "string", "minlength": 6, "maxlength": 64, "required": True},
+    "role": {"type": "string", "required": True, "allowed": ["buyer", "seller"]},
 }
