d9c3bf610160b3ca6a5edb69c8e557fdd1c4db74
Owent Ovandy <owent.ovandy7777@gmail.com>
2024-07-31 17:09:50 +0700

    added validation for user

---
+++ validations/user_register.py
@@ -0,0 +1,8 @@
+user_register_schema = {
+    "email": {
+        "type": "string",
+        "required": True,
+        "regex": "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
+    },
+    "password": {"type": "string", "minlength": 6, "maxlength": 64, "required": True},
+}
