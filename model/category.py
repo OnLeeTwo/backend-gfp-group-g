945103c02039cc3b6e98ef7d3e8588f5e6660863
firman maulana <firmanasgani37@gmail.com>
2024-08-02 23:25:05 +0700

    adding model

--- model/category.py
+++ model/category.py
@@ -3,6 +3,6 @@ from sqlalchemy import String
 from sqlalchemy.orm import mapped_column
 
 class Category(Base):
-    __tablename__ = 'category'
+    __tablename__ = "category"
     id = mapped_column(String(10), primary_key=True)
-    name = mapped_column(String,nullable=False)
+    name = mapped_column(String(100),nullable=False)
