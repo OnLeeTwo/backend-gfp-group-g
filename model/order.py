bb72a2db05361b54cc32d4c40f1a382e9b70c556
Owent Ovandy <owent.ovandy7777@gmail.com>
2024-08-02 19:33:54 +0700

    added order and promotion model

---
+++ model/order.py
@@ -0,0 +1,32 @@
+from sqlalchemy import String, Integer, DECIMAL, DateTime, Enum, Text, ForeignKey, func
+from sqlalchemy.orm import mapped_column
+from model.base import Base
+import enum
+
+
+class OrderStatus(enum.Enum):
+    pending = "Pending"
+    shipping = "Shipping"
+    delivered = "Delivered"
+    arrived = "Arrived"
+
+
+class PaymentStatus(enum.Enum):
+    pending = "Pending"
+    done = "Done"
+
+
+class Order(Base):
+    __tablename__ = "order"
+
+    order_id = mapped_column(String(10), primary_key=True, autoincrement=True)
+    user_id = mapped_column(String(10), ForeignKey("users.user_id"), nullable=False)
+    total_amount = mapped_column(DECIMAL, nullable=False)
+    created_at = mapped_column(
+        DateTime(timezone=True), nullable=False, server_default=func.now()
+    )
+    created_by = mapped_column(Integer, nullable=False)
+    status_order = mapped_column(Enum(OrderStatus), nullable=False)
+    status_payment = mapped_column(Enum(PaymentStatus), nullable=False)
+    shipping_address = mapped_column(Text, nullable=False)
+    promotion_id = mapped_column(Integer, ForeignKey("promotion.promotion_id"))
---
+++ model/promotion.py
@@ -0,0 +1,18 @@
+from sqlalchemy import String, Integer, DECIMAL, DateTime, Date, func
+from sqlalchemy.orm import mapped_column
+from model.base import Base
+
+
+class Promotion(Base):
+    __tablename__ = "promotions"
+
+    promotion_id = mapped_column(String(10), primary_key=True)
+    market_id = mapped_column(String(10), nullable=False)
+    code = mapped_column(String(255), unique=True, nullable=False)
+    discount_value = mapped_column(DECIMAL, nullable=False)
+    created_at = mapped_column(
+        DateTime(timezone=True), nullable=False, server_default=func.now()
+    )
+    updated_at = mapped_column(DateTime(timezone=True), default=None)
+    start_date = mapped_column(Date, nullable=False)
+    end_date = mapped_column(Date, nullable=False)
