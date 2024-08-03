57b3fa75d66f0988512b870c6a86fcb21bb94a1e
Owent Ovandy <84507533+OnLeeTwo@users.noreply.github.com>
2024-08-03 13:42:10 +0700

    Change seller model

--- model/seller.py
+++ model/seller.py
@@ -7,5 +7,5 @@ from sqlalchemy import Integer, String
 class Seller(Base):
     __tablename__ = "seller"
 
-    seller_id = mapped_column(Integer, primary_key=True)
+    seller_id = mapped_column(String(10), primary_key=True)
     user_id = mapped_column(String(10), nullable=False)
