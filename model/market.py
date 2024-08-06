92dc2fdc84641f4534e74073b44954536502a6b6
Richo Wardiyanto <richowardiyanto1@gmail.com>
2024-08-06 16:45:03 +0700

    rename variable seller_id

--- model/market.py
+++ model/market.py
@@ -7,7 +7,7 @@ class Market(Base):
     __tablename__ = "market"
 
     market_id = mapped_column(String(10), primary_key=True)
-    seller_id = mapped_column(Integer, ForeignKey('seller.seller_id'), nullable=False)
+    seller_id = mapped_column(String(10), ForeignKey('seller.seller_id'), nullable=False)
     name = mapped_column(String(255), nullable=False)
     location = mapped_column(Text, nullable=True)
     created_at = mapped_column(DateTime, nullable=False, server_default=func.now())
